519a95f44de804eee5f9ead910b3927bb54f36fb
Sumit Ahuja <sahuja@argusoft.com>
2023-02-22 23:04:59 +0530

    Added chefRequestedClarification flag in order

--- src/order/dto/chef-inquiry.dto.ts
+++ src/order/dto/chef-inquiry.dto.ts
@@ -1,10 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class ChefInquiryDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
   comment: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsBoolean()
+  chefRequestedClarification?: boolean;
 }
