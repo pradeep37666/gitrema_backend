7c7c59d797c0792c36834b518620a9382daf4cd2
Sumit Ahuja <sahuja@argusoft.com>
2023-02-25 14:58:52 +0530

    Added Websocket Auth guard module

---
+++ src/socket-io/guards/ws-jwt-auth.guard.ts
@@ -0,0 +1,52 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  Logger,
+  forwardRef,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { WsException } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { SocketIoGateway } from '../socket-io.gateway';
+import { SocketEvents } from '../enum/events.enum';
+
+@Injectable()
+export class WsJwtGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    @Inject(forwardRef(() => SocketIoGateway))
+    private socketGateway: SocketIoGateway,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const client: Socket = context.switchToWs().getClient<Socket>();
+
+    try {
+      const authTokenArr = client.handshake?.headers?.authorization?.split(' ');
+
+      if (authTokenArr.length != 2) {
+        this.socketGateway.emit(client.id, SocketEvents.auth, false);
+        client.disconnect();
+
+        throw new WsException(`Invalid Token`);
+      }
+      const authToken: string = authTokenArr[1];
+      const payload: any = await this.jwtService.decode(authToken);
+      if (payload.userId) {
+        this.socketGateway.emit(client.id, SocketEvents.auth, true);
+        context.switchToHttp().getRequest().user = payload;
+        client.data.user = payload;
+        if (payload.supplierId && payload.roleId)
+          client.join(`${payload.supplierId}_${payload.roleId}`);
+        return true;
+      }
+      return false;
+    } catch (err) {
+      this.socketGateway.emit(client.id, SocketEvents.auth, false);
+      client.disconnect();
+      throw new WsException(`Auth Unsuccessfull`);
+    }
+  }
+}
