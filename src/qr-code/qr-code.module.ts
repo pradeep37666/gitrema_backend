a5f86c5993f37623cba86ed4169536bc1df47e64
Sumit Ahuja <sahuja@argusoft.com>
2023-02-25 14:56:37 +0530

    Implemented QR code lib and made it either create or update for same dataId

--- src/qr-code/qr-code.module.ts
+++ src/qr-code/qr-code.module.ts
@@ -3,10 +3,16 @@ import { QrCodeService } from './qr-code.service';
 import { QrCodeController } from './qr-code.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QrCode, QrCodeSchema } from './schemas/qr-code.schema';
+import { Table, TableSchema } from 'src/table/schemas/table.schema';
+import { StorageModule } from 'src/core/Providers/Storage/storage.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: QrCode.name, schema: QrCodeSchema }]),
+    StorageModule,
+    MongooseModule.forFeature([
+      { name: QrCode.name, schema: QrCodeSchema },
+      { name: Table.name, schema: TableSchema },
+    ]),
   ],
   controllers: [QrCodeController],
   providers: [QrCodeService],
