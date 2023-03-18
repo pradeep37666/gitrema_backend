6153cb1a46e836351fbb722dbee2886f052df3b7
Sumit Ahuja <sahuja@argusoft.com>
2023-03-18 10:12:18 +0530

    Implemented a basic structure for whatsapp notification. Trigerred notification for pickup order create and order ready

---
+++ src/order/order-notification.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@nestjs/common';
+
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { OrderDocument } from './schemas/order.schema';
+
+import { OrderStatus, OrderType } from './enum/en.enum';
+
+import { replaceAll } from 'src/core/Helpers/universal.helper';
+
+import {
+  Customer,
+  CustomerDocument,
+} from 'src/customer/schemas/customer.schema';
+import { WhatsappService } from 'src/core/Providers/http-caller/whatsapp.service';
+import { OrderNotifications } from './constant/whatsapp-templates.constant';
+
+@Injectable()
+export class OrderNotificationService {
+  constructor(
+    @InjectModel(Customer.name)
+    private readonly customerModel: Model<CustomerDocument>,
+    private readonly whatsappService: WhatsappService,
+  ) {}
+
+  async triggerOrderNotification(order: OrderDocument) {
+    // send whatsapp notification for pickup order
+    let template = null;
+    if (order.orderType == OrderType.Pickup) {
+      switch (order.status) {
+        case OrderStatus.New:
+          template =
+            OrderNotifications[OrderType.Pickup]?.OrderCreateNotification['en'];
+          break;
+        case OrderStatus.DonePreparing:
+          template =
+            OrderNotifications[OrderType.Pickup]?.OrderReadyNotification['en'];
+          break;
+      }
+      if (template) {
+        await this.triggerWhatsappNotification(order, template);
+      }
+    }
+  }
+
+  async triggerWhatsappNotification(order: OrderDocument, template: string) {
+    let phoneNumber = order.contactNumber;
+
+    let customer = null;
+    if (!phoneNumber && order.customerId) {
+      customer = await this.customerModel.findById(order.customerId);
+      if (customer) {
+        phoneNumber = customer.phoneNumber;
+      }
+    }
+    if (phoneNumber) {
+      template = this.prepareMessage(order, template, customer);
+      const response = await this.whatsappService.sendMessage(
+        order.supplierId.toString(),
+        phoneNumber,
+        template,
+      );
+
+      console.log(`Whats app message status ---`, {
+        supplierId: order.supplierId.toString(),
+        phoneNumber,
+        template,
+      });
+    }
+  }
+  prepareMessage(
+    order: OrderDocument,
+    template: string,
+    customer: CustomerDocument,
+  ) {
+    const wordsToReplace = {
+      '{{CustomerName}}': customer ? customer.name : order.name,
+      '{{OrderNumber}}': order.orderNumber,
+    };
+    return replaceAll(template, wordsToReplace);
+  }
+}
